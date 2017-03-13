f7213ff8ff45b0e6d073b2346a05ae06c9fa8f08
Dmytro Martyniuk <dmytro.martyniuk09@gmail.com>
2017-03-14 00:43:56 +0100

    Added fibonacci recursive method func1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    public static int func1(int con) {
+        if (con == 2 || con == 1)
+            return 1;
+        return func1(con-1) + func1(con-2);
+    }
 
     public static void func2(int con) {
         int num1 =0;
@@ -17,11 +21,27 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-            System.out.print("Input number, condition: ");
+        System.out.println("1 - List of N Fibonacci sequence");
+        System.out.println("2 - List of N-th Fibonacci sequence");
+        int tmp = sc.nextInt();
+        switch (tmp) {
+            case 1: {
+                System.out.print("Input number, condition1: ");
+                int con = sc.nextInt();
+                System.out.print(0+", ");
+                for(int i =1; i<= con; i++) {
+                    System.out.print(func1(i)+", ");
+                }
+                break;
+            }
+            case 2: {
+                System.out.print("Input number, condition2: ");
                 int con = sc.nextInt();
                 func2(con);
+                break;
+            }
+        }
     }
 }
 
